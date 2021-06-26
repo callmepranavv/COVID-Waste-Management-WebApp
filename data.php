384d8002a0641516b1ec55e1596908297bee2c95
Pranav Shrivastava <63885865+callmepranavv@users.noreply.github.com>
2021-06-26 23:52:43 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+
+<title>WELCOME</title>
+<link rel="stylesheet" href="css/style2.css">
+
+</style>
+</head>
+<style>
+	body{
+		
+		background-image : url('imgs/cov.jpg');
+		background-repeat : no-repeat;
+		background-attachment: fixed;
+ 		background-size: cover;
+	}
+</style>
+<body>
+ <h1></h1>
+ <div id='main'>
+		
+</div>	
+		<form class="myform" action="create.php"method="post">
+		<a href="order.php"><input type="button" id="a" value="Orders |_<_|"/></a>
+		<a href="response.php"><input type="button" id="b" value="Pick-up's |_<_|"/></a>
+		<a href="home.php"><input type="button" id="c" value="Orders |_<_|"/></a>
+		<a href="login.php"><input type="button" id="e" value="Exit :))"/></a>
+		</form>
+</body>
+</html>
+
+
+
+
