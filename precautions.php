d53788d548798390c3d7340a99752327c741fc00
Pranav Shrivastava <63885865+callmepranavv@users.noreply.github.com>
2021-06-29 23:08:01 +0530

    Add files via upload

---
+++ precautions.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<style>
+	body{
+		
+		background-color : #f5f6fa;
+		background-repeat : no-repeat;
+		background-attachment: fixed;
+ 		background-size: cover;
+	}
+</style>
+<body>
+<div id="d1">
+<h1 id="text">🆆🅷🅰🆃 🆃🅾 🅳🅾 ?</h1>
+		<img  src="imgs/virus1.png" alt="logo" height="200" width="200">
+		<center>
+		<img id="p" src="imgs/name.png"  height="190" width="750">
+		</center>
+		<img id="p1"  src="imgs/virus1.png" alt="logo" height="200" width="200">
+		</div>
+    <div id="d2"></div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+
+	<button id="dark" onclick="myFunction()"></button>
+<div class="cmp-text">
+
+    <h1 style="color:#EA2027">Care Options<br></h1>
+<h2 style="color:#0097e6">Self Care</h2>
+<p>
+Stay aware of the latest information on the COVID-19 outbreak, available on the WHO website and through your national and local public health authority. Most countries around the world have seen cases of COVID-19 and many are experiencing outbreaks. Authorities in China and some other countries have succeeded in slowing their outbreaks. However, the situation is unpredictable so check regularly for the latest news.</p>
+<h3 style="color:#EA2027">Protecting yourself and others from the spread COVID-19</h3>
+<p style="color:#0097e6" >You can reduce your chances of being infected or spreading COVID-19 by taking some simple precautions:</p>
+<p style="color:#0097e6">Remember to take everyday preventive actions that are always recommended to prevent the spread of respiratory viruses.</p>
+<ul>
+<li>Regularly and thoroughly clean your hands with an      alcohol-based hand rub or wash them with soap and water. Why? Washing your      hands with soap and water or using alcohol-based hand rub kills viruses      that may be on your hands.</li><br>
+<li>Maintain at least 1 metre (3 feet) distance between      yourself and others. Why? When someone coughs, sneezes, or speaks they      spray small liquid droplets from their nose or mouth which may contain      virus. If you are too close, you can breathe in the droplets, including      the COVID-19 virus if the person has the disease.</li><br>
+<li>Avoid going to crowded places. Why? Where people come      together in crowds, you are more likely to come into close contact with      someone that has COIVD-19 and it is more difficult to maintain physical      distance of 1 metre (3 feet).</li><br>
+<li>Avoid touching eyes, nose and mouth. Why? Hands touch      many surfaces and can pick up viruses. Once contaminated, hands can      transfer the virus to your eyes, nose or mouth. From there, the virus can      enter your body and infect you.</li><br>
+<li>Make sure you, and the people around you, follow good      respiratory hygiene. This means covering your mouth and nose with your      bent elbow or tissue when you cough or sneeze. Then dispose of the used      tissue immediately and wash your hands. Why? Droplets spread virus. By      following good respiratory hygiene, you protect the people around you from      viruses such as cold, flu and COVID-19.</li><br>
+<li>Stay home and self-isolate even with minor symptoms      such as cough, headache, mild fever, until you recover. Have someone bring      you supplies. If you need to leave your house, wear a mask to avoid      infecting others. Why? Avoiding contact with others will protect them from      possible COVID-19 and other viruses.</li><br>
+<li>If you have a fever, cough and difficulty breathing,      seek medical attention, but call by telephone in advance if possible and      follow the directions of your local health authority. Why? National and      local authorities will have the most up to date information on the      situation in your area. Calling in advance will allow your health care      provider to quickly direct you to the right health facility. This will      also protect you and help prevent spread of viruses and other infections.</li><br>
+<li>Keep up to date on the latest information from trusted      sources, such as WHO or your local and national health authorities. Why?      Local and national authorities are best placed to advise on what people in      your area should be doing to protect themselves.</li><br>
+</ul>
+
+<h2 style="color:#EA2027">If You Feel Sick</h2>
+<p>If you feel sick with fever/chills, cough, sore throat, difficulty breathing, other flu-like symptoms or were in close contact with someone with COVID-19 in the 14 days before you began to feel sick, seek medical care. Before you go to a doctor’s office or emergency room, call ahead and tell them about your possible exposure, recent travel and your symptoms.&nbsp;</p>
+<h2 style="color:#EA2027">Stay Informed</h2>
+<p>Our hospitals are closely monitoring the coronavirus (COVID-19) outbreak and are in close contact with local health authorities. For more information for the public and health care providers on coronavirus, please visit the Health Care website</a>.</p>
+</div>
+
+<script>
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
+
+</body>
+</html>
