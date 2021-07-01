061b29022aa5709ed5a61736c434e7f6aee7fcad
Pranav Shrivastava <63885865+callmepranavv@users.noreply.github.com>
2021-07-02 01:25:26 +0530

    Add files via upload

---
+++ wmanage.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Waste Management</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<style>
+	body{
+		
+		background-color : #f5f6fa;
+		background-repeat : no-repeat;
+		background-attachment: fixed;
+ 		background-size: cover;
+	}
+</style>
+<body>
+<div id="d1">
+		<img  src="imgs/virus1.png" alt="logo" height="200" width="200">
+		<center>
+		<img id="p" src="imgs/name.png"  height="190" width="750">
+		</center>
+		<img id="p1"  src="imgs/virus1.png" alt="logo" height="200" width="200">
+		</div>
+    <div id="d2"></div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+
+	<button id="dark" onclick="myFunction()"></button>
+    <h1 id="text1">🆃🅸🅿🆂 🅾🅽 🆂🅰🅵🅴🅻🆈 🅳🅸🆂🅿🅾🆂🅸🅽🅶 🅾🅵 🅼🅴🅳🅸🅲🅰🅻 🆆🅰🆂🆃🅴</h1>
+<div class="cmp-text" itemprop="articleBody">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
+</script>
+
+<p >When you run a healthcare facility, properly disposing of medical waste plays an important role in ensuring the safety and overall health of staff, patients and visitors. With the amount of waste produced each day, healthcare offices must be constantly prepared, or else something could go wrong during any phase of the disposal process. Check out the infographic below for an overview of how to keep your facility safe, clean and organized at all times, and then continue reading below for more information:</p>
+<p><span id="more-492"></span></p>
+
+<p><strong style="color:#EA2027" >1. Leave it to the professionals. </strong>Considering how the Department of Transportation has strict regulations about who has authorization to move medical waste, if you were to do it yourself and something were to go wrong, there would be consequences to pay. Medical waste must be packaged in leak proof containers that meet specific puncture and drop tests. Turn to a team of professionals instead, like </p>
+<p><strong style="color:#EA2027" >2. Secure storage. </strong>You don’t want clutter when it comes to medical waste; make sure that everything goes into a labeled container, because anything that touches medical waste becomes medical waste itself. To prevent contamination, it’s necessary to set up a clearly labeled and organized medical waste disposal system.</p>
+<p><strong style="color:#EA2027" >3. Keep pharmaceuticals separate</strong>; they require a different type of treatment than standard regulated medical waste. When you go with Our medical Services, you can take advantage of the mail back pharmaceutical destruction program. With this, they go through another partner to ensure the safe, secure destruction of all hazardous pharmaceuticals. With their team of experts, you can trust that they’ll follow vigorously monitored regulations the whole way through.</p>
+<p><strong style="color:#EA2027" >4. Organize training. </strong>Make sure you inform the medical professionals employed at your facility about the safe handling of blood borne pathogens while on the job. Go over the detailed guidelines set by the Occupational Safety and Health Administration (OSHA) at least once a year to increase awareness and stress the importance of properly disposing of waste. If your business would like to simplify this process, call Choice MedWaste to ask about their online compliance training software that can organize all the training your staff needs.</p>
+<p><strong style="color:#EA2027" >5. Be careful with hazardous waste. </strong>Make sure you don’t mix chemicals, aerosols or any other hazardous waste with medical waste; just like pharmaceuticals, the guidelines set for hazardous waste are also much different than standard red bag medical waste. &nbsp;If you aren’t sure if an item is hazardous or not, contact Choice MedWaste and they can help identify the proper way to safely dispose of your special waste.</p>
+<p>Reduce the risks of mishandling medical waste by keeping everyone aware of the proper procedures. Whenever you have a streamlined plan in place, you have more peace of mind about the safety of everyone involved! View the video below to see why Choice Med Waste is the top choice for proper medical disposal:</p>
+<div style="text-align: center;"><iframe src="https://www.youtube.com/embed/_woilVD8IRA" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
+
+
+<button id="hide">Hide</button>
+<button id="show">Show</button>
+<h3 style="color:#EA2027">If you have wastes like Masks,gloves or any Medical Waste which Cannot be thrown wherever you want.Kindly Register with We will do it for you. :)))<a href="waste.php"><input type="button" id="back" name="submit_btn" value="Register Here"/></a></h3><br>
+
+</div>
+<script>
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
+
+</body>
+</html>
